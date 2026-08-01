74d3194360f34349c2580f3268efe2091392a198
Akash Popatrao Shejul <114986606+akash0117P@users.noreply.github.com>
2026-08-01 19:36:16 +0530

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.java
@@ -0,0 +1,64 @@
+class Node {
+    Node[] links = new Node[26];
+    boolean fl;
+
+    public Node() {}
+
+    boolean containsKey(char ch) {
+        if (links[ch - 'a'] == null) {
+            return false;
+        }
+        return true;
+    }
+    void put(char ch, Node temp) {
+        links[ch - 'a'] = temp;
+        return;
+    }
+
+    Node get(char ch) {
+        return links[ch - 'a'];
+    }
+    void setEnd() {
+        fl = true;
+    }
+}
+
+class PrefixTree {
+    Node root;
+    public PrefixTree() {
+        root = new Node();
+    }
+
+    public void insert(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (!curr.containsKey(word.charAt(i))) {
+                curr.put(word.charAt(i), new Node());
+            }
+            curr = curr.get(word.charAt(i));
+        }
+        curr.setEnd();
+    }
+
+    public boolean search(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (!curr.containsKey(word.charAt(i))) {
+                return false;
+            }
+            curr = curr.get(word.charAt(i));
+        }
+        return curr.fl;
+    }
+
+    public boolean startsWith(String prefix) {
+        Node curr = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            if (curr == null || !curr.containsKey(prefix.charAt(i))) {
+                return false;
+            }
+            curr = curr.get(prefix.charAt(i));
+        }
+        return true;
+    }
+}
