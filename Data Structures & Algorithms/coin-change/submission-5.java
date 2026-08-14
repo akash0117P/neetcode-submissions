7b924c12215b6bbccb69aeba915b8cb1ab39eb75
Akash Popatrao Shejul <114986606+akash0117P@users.noreply.github.com>
2026-08-14 21:42:52 +0530

    Add: coin-change - submission-5

---
+++ Data Structures & Algorithms/coin-change/submission-5.java
@@ -0,0 +1,35 @@
+class Solution {
+    int[][] dp;
+    int countWays(int i, int taken, int sum, int amount, int[] coins) {
+        if (sum == amount) {
+            return 0;
+        }
+        if (i < 0 || sum > amount) {
+            return Integer.MAX_VALUE;
+        }
+        if (dp[i][sum] != -1)
+            return dp[i][sum];
+
+        int take = countWays(i, taken + 1, sum + coins[i], amount, coins);
+        if (take != Integer.MAX_VALUE) {
+            take++;
+        }
+        int notake = countWays(i - 1, taken, sum, amount, coins);
+
+        return dp[i][sum] = Math.min(take, notake);
+    }
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+
+        dp = new int[n][amount];
+        for (int i = 0; i < n; i++) {
+            java.util.Arrays.fill(dp[i], -1);
+        }
+
+        if (amount == 0) {
+            return 0;
+        }
+        int ans = countWays(n - 1, 0, 0, amount, coins);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
