6e1fa557c54223b037ce1e93e009870ea68ffc81
Akash Popatrao Shejul <114986606+akash0117P@users.noreply.github.com>
2026-08-14 21:09:07 +0530

    Add: decode-ways - submission-5

---
+++ Data Structures & Algorithms/decode-ways/submission-5.java
@@ -0,0 +1,51 @@
+class Solution {
+    int[] dp;
+    int countWays(int i, String s) {
+        if (i == 0)
+            return 1;
+        if (i < 0)
+            return 0;
+
+        if (dp[i] != 0)
+            return dp[i];
+
+        int res = 0;
+
+        if (s.charAt(i - 1) != '0') {
+            res += countWays(i - 1, s);
+        }
+
+        if (i >= 2) {
+            int num = Integer.parseInt(s.substring(i - 2, i));
+            if (num >= 10 && num <= 26) {
+                res += countWays(i - 2, s);
+            }
+        }
+
+        return dp[i] = res;
+    }
+    public int numDecodings(String s) {
+        int n = s.length();
+        dp = new int[n + 1];
+
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            int res = 0;
+            if (s.charAt(i - 1) != '0') {
+                res += dp[i - 1];
+            }
+
+            if (i >= 2) {
+                int num = Integer.parseInt(s.substring(i - 2, i));
+                if (num >= 10 && num <= 26) {
+                    res += dp[i - 2];
+                }
+            }
+
+            dp[i] = res;
+        }
+
+        return dp[n];
+    }
+}
