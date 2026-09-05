a7bfb2fd44a541f23128cee21abfa6dd7774a054
Akash Popatrao Shejul <114986606+akash0117P@users.noreply.github.com>
2026-09-05 16:35:18 +0530

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (k == 1) {
+            return nums;
+        }
+        Deque<Integer> dq = new LinkedList<>();
+        int n = nums.length;
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.pollFirst();
+            }
+            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+
+            if (i >= k - 1) {
+                res.add(nums[dq.peekFirst()]);
+            }
+        }
+        return res.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
