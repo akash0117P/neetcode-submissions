27c11c66435c5e2171828a86b5967576347bc119
Akash Popatrao Shejul <114986606+akash0117P@users.noreply.github.com>
2026-09-06 19:54:30 +0530

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        int[][] pair = new int[n][2];
+        double prevTime = 0;
+        int fleets = 0;
+
+        for (int i = 0; i < n; i++) {
+            pair[i][0] = position[i];
+            pair[i][1] = speed[i];
+        }
+        Arrays.sort(pair, (a, b) -> Integer.compare(b[0], a[0]));
+
+        for (int i = 0; i < n; i++) {
+            double time = (double) (target - pair[i][0]) / pair[i][1];
+            if (time > prevTime) {
+                fleets++;
+                prevTime = time;
+            }
+        }
+        return fleets;
+    }
+}
